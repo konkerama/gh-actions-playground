863680e88823915136a326fef6b3cbaaeb0e57f2
konkeramaris <konkeramaris@gmail.com>
2023-01-13 23:38:40 +0100

    fix: pylint

--- src/sample-function/index.py
+++ src/sample-function/index.py
@@ -1,8 +1,16 @@
+# pylint: disable=line-too-long
+
+''' Sample lambda function '''
 import requests
 
+URL = "https://raw.githubusercontent.com/konkerama/konkerama.github.io/master/contact-info/index.json"
+
 def lambda_handler(event, context):
-    response = requests.get("https://raw.githubusercontent.com/konkerama/konkerama.github.io/master/contact-info/index.json")
+    ''' Lambda Handler '''
+    response = requests.get(url=URL)
     print (response.text)
+    print (event)
+    print (context)
     return "Hello World"
 
 if __name__=="__main__":
