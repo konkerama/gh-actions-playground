2ea76898c6fdf9900af82609a5f2025dfb8e0a20
konkeramaris <konkeramaris@gmail.com>
2023-01-14 19:11:14 +0100

    feat: modified cicd

--- terraform-manifests/main.tf
+++ terraform-manifests/main.tf
@@ -3,7 +3,7 @@ module "lambda_function" {
 
   function_name = "${var.resource_name}-${var.environment}"
   description   = "My awesome lambda function"
-  handler       = "index.lambda_handler"
+  # handler       = "index.lambda_handler"
   runtime       = "python3.8"
   memory_size   = var.memory
 
@@ -13,10 +13,15 @@ module "lambda_function" {
   }
 
 
+   s3_existing_package = {
+     bucket = var.lambda-artifact-s3-bucket
+     key = var.lambda-artifact-s3-key
+   }
 
-  var.resource_name
 
-  source_path = "../src/sample-function"
+  # var.resource_name
+
+  # source_path = "../src/sample-function"
 
   tags = {
     Name = "my-lambda1"
