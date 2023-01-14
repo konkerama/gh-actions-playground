77527f96ffb19e81868c6b10f2bdba1ed08dfd59
konkeramaris <konkeramaris@gmail.com>
2023-01-14 16:14:34 +0100

    test

--- src/sample-function/test_index.py
+++ src/sample-function/test_index.py
@@ -1,7 +1,10 @@
 '''Python Tests'''
 import index
+import os
+
+ENV = os.environ['ENV']
 
 def test_lambda_handler():
     '''Verify the output of lambda_handler function'''
     output = index.lambda_handler("","")
-    assert output == "Hello World"
+    assert output == "env="+ENV
