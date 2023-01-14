6a20e1ffa7414e96d5a5c457c2ea2227a66f0229
konkeramaris <konkeramaris@gmail.com>
2023-01-14 16:12:00 +0100

    test: environment in github actions

--- src/sample-function/index.py
+++ src/sample-function/index.py
@@ -1,13 +1,17 @@
 # pylint: disable=line-too-long
 
 ''' Sample lambda function '''
+import os
 import requests
+
+ENV = os.environ['ENV']
 
 URL = "https://raw.githubusercontent.com/konkerama/konkerama.github.io/master/contact-info/index.json"
 
 def lambda_handler(event, context):
     ''' Lambda Handler '''
     response = requests.get(url=URL, timeout=10)
+    print (ENV)
     print (response.text)
     print (event)
     print (context)
